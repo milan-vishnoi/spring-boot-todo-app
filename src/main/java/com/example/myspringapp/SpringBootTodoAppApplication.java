4d32e503e0e5cc82fd4affa2cdcd5580ff9bc0a8
Milan Vishnoi <milan.vish10@gmail.com>
2026-05-18 22:26:10 +0530

    Added the Bean in the main class to expose the mcp tools with the help of ToolCallbackProvider

--- src/main/java/com/example/myspringapp/SpringBootTodoAppApplication.java
+++ src/main/java/com/example/myspringapp/SpringBootTodoAppApplication.java
@@ -1,13 +1,24 @@
 package com.example.myspringapp;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.myspringapp.todo.mcp.TodoMcpTools;
 
 @SpringBootApplication
 public class SpringBootTodoAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTodoAppApplication.class, args);
+    }
+
+    @Bean
+    ToolCallbackProvider toolCallbackProvider(TodoMcpTools todoMcpTools) {
+        return MethodToolCallbackProvider.builder().toolObjects(todoMcpTools).build();
+
     }
 
 }
